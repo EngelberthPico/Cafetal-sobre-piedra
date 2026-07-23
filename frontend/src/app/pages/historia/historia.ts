492349d786a0031a49ef84ac9a83b502fde02a4d
Engelberth Sneyder Pico Blanco <engelberthyam@gmail.com>
2026-07-22 23:00:09 -0500

    Construir página de Nuestra Historia con contenido real y ajustes de diseño

--- frontend/src/app/pages/historia/historia.ts
+++ frontend/src/app/pages/historia/historia.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+ import { RouterLink } from '@angular/router';
 
-@Component({
-  selector: 'app-historia',
-  imports: [],
-  templateUrl: './historia.html',
-  styleUrl: './historia.css',
-})
-export class Historia {}
+  @Component({
+    selector: 'app-historia',
+   imports: [RouterLink],
+    templateUrl: './historia.html',
+    styleUrl: './historia.css',
+  })
+  export class Historia {}
